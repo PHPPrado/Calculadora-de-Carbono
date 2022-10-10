60468a4c46d979a4458fa6f9f445ead18db1bcd1
Pedro <pedro.phprado@gmail.com>
2022-10-10 00:00:55 -0300

    Cuidando da parte estetica

--- Programa.py
+++ Programa.py
@@ -1,12 +1,15 @@
-#import os
-# os.system('cls')
+import os
+os.system('cls')
+
+print("Bem vindo, com este programa você poderá ter acesso aos índices de emissões de CO2 do seu empreendimento, ou de sua pessoa física.\n")
+
 
 # Cálculo do consumo de energia----------
 
-# Variáveis----------
+print("Calculo do consumo de energia de um equipamento de mineração: \n")
 potencia = 850
 horas = int(
-    input("Digite a média de horas diárias que a máquina ficará ligada: "))
+    input("Digite a quantidade de horas diárias que a máquina ficará ligada: "))
 dias = 30
 consumoKwhM = (potencia * horas * dias) / 1000  # Equação do consumo
 
@@ -18,11 +21,13 @@ print("O consumo mensal do equipamento será de {} Kwh/mês" .format(consumoKwhM
 tarifa = 0.68
 custoTotal = consumoKwhM * tarifa  # Valor mensal consumido pela máquina
 
-print("O custo de energia mensal consumida por um equipamento será de: %2.2f" %
+print("O preço da energia mensal consumida por um equipamento de mineração será de: R$%2.2f \n" %
       round(custoTotal))
 
 
 # Cálculo da geração de energia fotovoltaica-----------
+
+print("Módulos fotovoltaicos ")
 
 potencia = 350
 horas = 5
@@ -31,7 +36,7 @@ energiaGerada = ((potencia * horas * rendimento) /
                  1000) * 30  # Energia mensal gerada por placa
 
 print("O rendimento mensal de um módulo fotovoltaico é de ",
-      int(energiaGerada), "kWh")
+      int(energiaGerada), "kWh \n")
 
 modulos = 0
 geracaoTotal = 0
@@ -39,28 +44,33 @@ while geracaoTotal < consumoKwhM:
     geracaoTotal += energiaGerada
     modulos += 1
 
-print("Serão necessários {} módulos foltovotaicos para suprir o consumo da mensal da máquina de {} kWh/mês" .format(
+print("Serão necessários {} módulos foltovotaicos para suprir o consumo da mensal da máquina mineradora de {} kWh/mês \n" .format(
     modulos, consumoKwhM))
 
 # Fator de emissão
 
+print("Emissão de carbono gerada pela máquina de mineração:")
+
 fatorEmissao = 0.1264
 carbonoEmitido = (consumoKwhM/1000) * fatorEmissao
-conversaoKwh = carbonoEmitido * 1000
+carbonoKg = carbonoEmitido * 1000
 
-print("Sua emissão proveniente do consumo de {}Kwh/mês, é de {} Kg de carbono.".format(consumoKwhM, conversaoKwh))
+print("Sua emissão proveniente do consumo de {} Kwh/mês, é de {} Kg de carbono. \n"
+      .format(consumoKwhM, round(carbonoKg, 2)))
 
 
 # Compensar emissão por reflorestamento------------
 
+print("Formas de compensar o carbono emitido pela máquina mineradora:")
+
 contadorArvores = 0
 arvore = 0
-while arvore <= conversaoKwh:
+while arvore <= carbonoKg:
     arvore += 7.4
     contadorArvores += 1
 
-print("Para compensar {} de carbono emitido, é necessário plantar {} árvores" .format(
-    conversaoKwh, contadorArvores))
+print("Para compensar {} quilos de carbono emitidos, é necessário plantar {} árvores" .format(
+    round(carbonoKg, 2), contadorArvores))
 
 
 # Calculo de créditos de carbono--------
@@ -68,10 +78,10 @@ print("Para compensar {} de carbono emitido, é necessário plantar {} árvores"
 creditos = 0
 preco = 0
 compensado = 0
-while compensado < conversaoKwh:
+while compensado < carbonoKg:
     creditos += 1
     preco += 3.00
     compensado += 1000
 
-print("Será necessário a compra de {} créditos de carbono por U${}.".format(
+print("Ou será necessário adquirir {} créditos de carbono por U${}.".format(
     creditos, preco))
