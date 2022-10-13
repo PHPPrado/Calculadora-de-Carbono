f3da0873409c42b51a2a7096a794b5b978f71561
Pedro <pedro.phprado@gmail.com>
2022-10-12 23:43:10 -0300

    Continuando funções e  criando menus inicias de interação com o usuário

--- Calculos.py
+++ Calculos.py
@@ -23,3 +23,41 @@ def quantArvores(consumoKwhM):
         arvore += 7.4
         contadorArvores += 1
     return contadorArvores
+
+
+def calculoCreditos(carbonoEmKg):
+    creditos = 0
+    preco = 0
+    compensado = 0
+    while compensado < carbonoEmKg:
+        creditos += 1
+        preco += 3.00
+        compensado += 1000
+
+    return creditos, preco
+
+
+def potenciaMineradora(tipoCalculo, potenciaMaquina):
+
+    if tipoCalculo == 1:
+        if potenciaMaquina == 1:
+            potencia = 3000
+        elif potenciaMaquina == 2:
+            potencia = 1800
+        elif potenciaMaquina == 3:
+            potencia = 850
+
+    elif tipoCalculo == 2:
+        if potenciaMaquina == 1:
+            potencia = 3000
+        elif potenciaMaquina == 2:
+            potencia = 1800
+        elif potenciaMaquina == 3:
+            potencia = 850
+
+    horas = int(
+        input("Digite a quantidade de horas diárias que a máquina ficará ligada: "))
+    dias = 30
+    consumoKwhM = (potencia * horas * dias) / 1000  # Equação do consumo
+
+    return potencia, consumoKwhM
--- Programa.py
+++ Programa.py
@@ -2,6 +2,7 @@ import os
 import Calculos
 os.system('cls')
 
+# ----------------------------------------------------------------------------------------------------------
 print("Bem vindo, com este programa você poderá ter acesso aos índices\nde emissões de CO2 do seu empreendimento, ou de sua pessoa física.\n")
 
 
@@ -10,31 +11,24 @@ tipoCalculo = int(input(
     "Qual tipo de cálculo você deseja realizar?\n\nMineração domestica.....................[1]\nMineração para meu empreendimento.......[2]\n\nDigite o número correspondente a sua escolha: "))
 
 
-# Cálculo para consumo domestico---------
+# Cálculo para consumo domestico
 if tipoCalculo == 1:
 
-    # Cálculo do consumo de energia----------
+    # Cálculo do consumo de energia (Residencial)----------
 
     print("\n----Calculo do consumo de energia de um equipamento de mineração----\n")
 
     # Escolher a potencia:
-    # /
-    # /
-    # /
-    # /
-    # /
 
-    potencia = 850
-    horas = int(
-        input("Digite a quantidade de horas diárias que a máquina ficará ligada: "))
-    dias = 30
-    consumoKwhM = (potencia * horas * dias) / 1000  # Equação do consumo
+    escolhaPotencia = int(input(
+        "Para calcular o seu consumo elétrico, selecione qual das opções melhor\nse encaixa na potência do seu equipamento de mineração:\n\nEquipamento de ALTA potência..........[1]\nEquipamento de MÉDIA potência.........[2]\nEquipamento de BAIXA potência.........[3]\n\nDigite o número correspondente: "))
+
+    potencia, consumoKwhM = Calculos.potenciaMineradora(
+        tipoCalculo, escolhaPotencia)
 
     print("\nO consumo mensal do equipamento será de {} Kwh/mês" .format(consumoKwhM))
 
-    # Cálculo do custo em reais---------
-
-    #
+    # Cálculo do custo em reais (Residencial)---------
 
     tarifa = 0.68
     custoTotal = consumoKwhM * tarifa  # Valor mensal consumido pela máquina
@@ -42,9 +36,10 @@ if tipoCalculo == 1:
     print("O preço da energia mensal consumida por um equipamento de mineração será de: R$%2.2f \n" %
           round(custoTotal))
 
-    # Cálculo da geração de energia fotovoltaica-----------
+    # Cálculo da geração de energia fotovoltaica (Residencial)-----------
 
-    print("Módulos fotovoltaicos ")
+    print("_________________________________________________________________________________________________")
+    print("Para economizar na conta de luz, recomendamos a instalação de módulos fotovoltaicos em sua residência.")
 
     potencia = 350
     horas = 5
@@ -53,46 +48,41 @@ if tipoCalculo == 1:
                      1000) * 30  # Energia mensal gerada por placa
 
     print("O rendimento mensal de um módulo fotovoltaico é de ",
-          int(energiaGerada), "kWh \n")
+          int(energiaGerada), "kWh. \n")
 
-    #modulos = 0
-    #geracaoTotal = 0
-    # while geracaoTotal < consumoKwhM:
-    #    geracaoTotal += energiaGerada
-    #   modulos += 1
-
-    print("Serão necessários {} módulos foltovotaicos para suprir o consumo da mensal da máquina mineradora de {} kWh/mês \n" .format(
+    # Calculo da quantidade de módulos fotovoltaicas necessárias (Residencial)
+    print("Serão necessários {} módulos fotovoltaicos para suprir o consumo da mensal de {} kWh/mês \n" .format(
         Calculos.modulosFotovoltaicos(consumoKwhM, energiaGerada), consumoKwhM))
 
-    # Fator de emissão
+    # Fator de emissão (Residencial)
 
+    print("_________________________________________________________________________________________________")
     print("Emissão de carbono gerada pela máquina de mineração:")
 
     print("Sua emissão proveniente do consumo de {} Kwh/mês, é de {} Kg de carbono. \n"
           .format(consumoKwhM, round(Calculos.calcFatorEmissao(consumoKwhM), 2)))
 
-    # Compensar emissão por reflorestamento------------
+    # Compensar emissão por reflorestamento (Residencial)------------
 
     print("Formas de compensar o carbono emitido pela máquina mineradora:")
 
     print("Para compensar {} quilos de carbono emitidos, é necessário plantar {} árvores" .format(
         round(Calculos.calcFatorEmissao(consumoKwhM), 2), Calculos.quantArvores(consumoKwhM)))
 
-    # Calculo de créditos de carbono--------
+    # Calculo de créditos de carbono (Residencial)--------
 
-    creditos = 0
-    preco = 0
-    compensado = 0
-    while compensado < Calculos.calcFatorEmissao(consumoKwhM):
-        creditos += 1
-        preco += 3.00
-        compensado += 1000
+    carbonoEmKg = Calculos.calcFatorEmissao(
+        consumoKwhM)  # Carbono emitido pela máquina
+    creditos, preco = Calculos.calculoCreditos(carbonoEmKg)
 
-    print("Ou será necessário adquirir {} créditos de carbono por U${}.".format(
+    print("ou será necessário adquirir {} créditos de carbono por U${}.".format(
         creditos, preco))
 
+# ------------------------------------------------------------------------------------------------------
+
+# Cálculo para consumo industrial
 elif tipoCalculo == 2:
-    # Cálculo do consumo de energia----------
+    # Cálculo do consumo de energia (Industrial) ----------
 
     print("\n----Calculo do consumo de energia de um equipamento de mineração----\n")
     potencia = 850
@@ -103,7 +93,7 @@ elif tipoCalculo == 2:
 
     print("O consumo mensal do equipamento será de {} Kwh/mês" .format(consumoKwhM))
 
-    # Cálculo do custo em reais---------
+    # Cálculo do custo em reais (Industrial) ---------
 
     tarifa = 0.68
     custoTotal = consumoKwhM * tarifa  # Valor mensal consumido pela máquina
@@ -111,7 +101,7 @@ elif tipoCalculo == 2:
     print("O preço da energia mensal consumida por um equipamento de mineração será de: R$%2.2f \n" %
           round(custoTotal))
 
-    # Cálculo da geração de energia fotovoltaica-----------
+    # Cálculo da geração de energia fotovoltaica (Industrial)-----------
 
     print("Módulos fotovoltaicos ")
 
@@ -124,48 +114,30 @@ elif tipoCalculo == 2:
     print("O rendimento mensal de um módulo fotovoltaico é de ",
           int(energiaGerada), "kWh \n")
 
-    #modulos = 0
-    #geracaoTotal = 0
-    # while geracaoTotal < consumoKwhM:
-    #    geracaoTotal += energiaGerada
-    #    modulos += 1
+    # Calculo quantidade de módulos fotovoltaicos (Industrial)---------------
 
     print("Serão necessários {} módulos foltovotaicos para suprir o consumo da mensal da máquina mineradora de {} kWh/mês \n" .format(
         Calculos.modulosFotovoltaicos(consumoKwhM, energiaGerada), consumoKwhM))
 
-    # Fator de emissão
+    # Fator de emissão (Industrial)
 
     print("Emissão de carbono gerada pela máquina de mineração:")
 
-    fatorEmissao = 0.1264
-    carbonoEmitido = (consumoKwhM/1000) * fatorEmissao
-    carbonoKg = carbonoEmitido * 1000
-
     print("Sua emissão proveniente do consumo de {} Kwh/mês, é de {} Kg de carbono. \n"
-          .format(consumoKwhM, round(carbonoKg, 2)))
+          .format(consumoKwhM, round(Calculos.calcFatorEmissao(consumoKwhM), 2)))
 
-    # Compensar emissão por reflorestamento------------
+    # Compensar emissão por reflorestamento (Industrial)------------
 
     print("Formas de compensar o carbono emitido pela máquina mineradora:")
 
-    contadorArvores = 0
-    arvore = 0
-    while arvore <= carbonoKg:
-        arvore += 7.4
-        contadorArvores += 1
-
     print("Para compensar {} quilos de carbono emitidos, é necessário plantar {} árvores" .format(
-        round(carbonoKg, 2), contadorArvores))
+        round(Calculos.calcFatorEmissao(consumoKwhM), 2), Calculos.quantArvores(consumoKwhM)))
 
-    # Calculo de créditos de carbono--------
+    # Calculo de créditos de carbono(Industrial)--------
 
-    creditos = 0
-    preco = 0
-    compensado = 0
-    while compensado < carbonoKg:
-        creditos += 1
-        preco += 3.00
-        compensado += 1000
+    carbonoEmKg = Calculos.calcFatorEmissao(
+        consumoKwhM)  # Carbono emitido pela máquina
+    creditos, preco = Calculos.calculoCreditos(carbonoEmKg)
 
-    print("Ou será necessário adquirir {} créditos de carbono por U${}.".format(
+    print("ou será necessário adquirir {} créditos de carbono por U${}.".format(
         creditos, preco))
